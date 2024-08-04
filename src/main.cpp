37576a27bdd42f33bf66571383c0100ef0371419
Reqwey <50829219+Reqwey@users.noreply.github.com>
2024-08-04 13:17:58 +0800

    feat: Add file management

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
+#include <algorithm>
 #include <csignal>
+#include <cstring>
+#include <fstream>
+#include <iostream>
 #include <ncurses.h>
-#include <string>
+#include <unistd.h>
 #include <vector>
 
 #define KEY_ESC 27
@@ -8,11 +12,15 @@
 #define CUS_COLOR_NUM 1
 
 enum EditorMode { NORMAL, INSERT, COMMAND };
+enum WrapMode { BREAK, SCROLL };
 
 class MiniVim {
 public:
-  MiniVim() {
-    initscr();                    // Start ncurses EditorMode
+  MiniVim(const std::string &filename, bool truncate, bool readOnly,
+          WrapMode wrapMode)
+      : filename(filename), truncate(truncate), readOnly(readOnly),
+        wrapMode(wrapMode) {
+    initscr();                    // Start ncurses mode
     raw();                        // Line buffering disabled
     keypad(stdscr, true);         // We get F1, F2, etc.
     noecho();                     // Don't echo while we do getch
@@ -43,17 +51,22 @@ public:
     wrefresh(commandWindow);
 
     cursorY = cursorX = 0;
-    EditorMode = NORMAL;
+    editorMode = NORMAL;
+
+    loadFile();
   }
 
   ~MiniVim() {
-    endwin(); // End ncurses EditorMode
+    endwin(); // End ncurses mode
   }
 
   void run() {
+    refreshScreen();
+
     int ch;
     while (ch = wgetch(stdscr)) {
-      switch (EditorMode) {
+      warnMessage = "";
+      switch (editorMode) {
       case NORMAL:
         handleNormalEditorMode(ch);
         break;
@@ -75,23 +88,71 @@ private:
   WINDOW *fileWindow;
   WINDOW *infoWindow;
   WINDOW *commandWindow;
-  EditorMode EditorMode;
+
+  EditorMode editorMode;
+
+  std::string filename;
+  bool truncate;
+  bool readOnly;
+  bool isNewFile;
+  bool modified;
+  WrapMode wrapMode;
+
   int cursorY, cursorX;
   int rows, cols;
   int startLine;
+
   char kbCache = '\0';
 
-  std::vector<std::string> buffer = {""};
+  std::string warnMessage;
+
+  std::vector<std::string> buffer;
   std::string commandBuffer;
+
+  void loadFile() {
+    if (truncate) {
+      buffer.push_back(""); // Start with an empty buffer if truncating
+      return;
+    }
+
+    std::ifstream file(filename);
+    if (file.is_open()) {
+      std::string line;
+      while (std::getline(file, line)) {
+        buffer.push_back(line);
+      }
+      file.close();
+      isNewFile = false;
+    } else {
+      buffer.push_back("");
+      isNewFile = true;
+      // Initialize with one empty line if file cannot be opened
+    }
+
+    modified = false;
+  }
+
+  void saveFile() {
+    std::ofstream file(filename);
+    if (file.is_open()) {
+      for (const auto &line : buffer) {
+        file << line << std::endl;
+      }
+      file.close();
+      modified = false;
+    } else {
+      warnMessage = "Failed to save file!";
+    }
+  }
 
   void handleNormalEditorMode(int ch) {
     switch (ch) {
     case 'i':
-      EditorMode = INSERT;
+      editorMode = INSERT;
       break;
 
     case ':':
-      EditorMode = COMMAND;
+      editorMode = COMMAND;
       commandBuffer.clear();
       break;
 
@@ -99,7 +160,7 @@ private:
       if (cursorY + startLine > 0) {
         --cursorY;
         cursorX =
-            std::min(cursorX, int(buffer[cursorY + startLine].length() - 1));
+            std::min(cursorX, std::max(0, int(buffer[cursorY + startLine].length() - 1)));
       }
       break;
 
@@ -107,7 +168,7 @@ private:
       if (cursorY + startLine < buffer.size() - 1) {
         ++cursorY;
         cursorX =
-            std::min(cursorX, int(buffer[cursorY + startLine].length() - 1));
+            std::min(cursorX, std::max(0, int(buffer[cursorY + startLine].length() - 1)));
       }
       break;
 
@@ -186,11 +247,12 @@ private:
   void handleInsertEditorMode(int ch) {
     switch (ch) {
     case KEY_ESC:
-      EditorMode = NORMAL;
+      editorMode = NORMAL;
       break;
 
     case KEY_BACKSPACE:
     case 127:
+      modified = true;
       if (!buffer[cursorY + startLine].empty() && cursorX > 0) {
         buffer[cursorY + startLine].erase(cursorX - 1, 1);
         --cursorX;
@@ -203,6 +265,7 @@ private:
       break;
 
     case KEY_DC:
+      modified = true;
       if (!buffer[cursorY + startLine].empty() &&
           cursorX < buffer[cursorY + startLine].length() - 1) {
         buffer[cursorY + startLine].erase(cursorX + 1, 1);
@@ -215,6 +278,7 @@ private:
 
     case KEY_ENTER:
     case '\n':
+      modified = true;
       buffer.insert(buffer.begin() + cursorY + startLine + 1,
                     buffer[cursorY + startLine].substr(cursorX));
       buffer[cursorY + startLine] =
@@ -276,6 +340,7 @@ private:
       break;
 
     default:
+      modified = true;
       buffer[cursorY + startLine].insert(cursorX, 1, ch);
       ++cursorX;
       break;
@@ -285,21 +350,38 @@ private:
   void handleCommandEditorMode(int ch) {
     switch (ch) {
     case KEY_ESC:
-      EditorMode = NORMAL;
+      editorMode = NORMAL;
       break;
+
     case KEY_BACKSPACE:
     case 127:
       if (commandBuffer.length()) {
         commandBuffer.pop_back();
       }
       break;
+
     case KEY_ENTER:
     case '\n':
-      if (commandBuffer == "q") {
-        endwin();
+      if (commandBuffer == "w") {
+        saveFile();
+      } else if (commandBuffer == "q") {
+        if (modified) {
+          warnMessage = "No write since last change (add ! to override)";
+        } else {
+          endwin(); // Properly end ncurses mode
+          exit(0);
+        }
+      } else if (commandBuffer == "q!") {
+        endwin(); // Properly end ncurses mode
         exit(0);
+      } else if (commandBuffer == "wq") {
+        saveFile();
+        endwin(); // Properly end ncurses mode
+        exit(0);
+      } else {
+        warnMessage = "Command not found.";
       }
-      EditorMode = NORMAL;
+      editorMode = NORMAL;
       break;
 
     default:
@@ -346,23 +428,33 @@ private:
     mvwin(commandWindow, rows - 1, 0);
     wrefresh(commandWindow);
 
-    mvwprintw(infoWindow, 0, 0,
-              "screen: %d %d, cursor: %d, %d, startline: %d\n", rows, cols,
-              cursorY, cursorX, startLine);
+    wclear(infoWindow);
+    if (warnMessage.length()) {
+      mvwprintw(infoWindow, 0, 0, "[WARN]%s\n", warnMessage.c_str());
+    } else {
+      mvwprintw(infoWindow, 0, 0, "\"%s\" %s\tLine %d\tCol %d\n",
+                filename.c_str(), isNewFile ? "(new file)" : "",
+                cursorY + startLine + 1, cursorX + 1);
+    }
     wrefresh(infoWindow);
 
     for (size_t i = 0; i < rows - 2 && i + startLine < buffer.size(); ++i) {
-      mvwprintw(fileWindow, i, 0, "%s", buffer[i + startLine].c_str());
+      mvwaddstr(fileWindow, i, 0, buffer[i + startLine].c_str());
+      // Wrap mode todo...
     }
+
     wrefresh(fileWindow);
 
-    if (EditorMode == COMMAND) {
+    if (editorMode == COMMAND) {
       mvwprintw(commandWindow, 0, 0, ":%s", commandBuffer.c_str());
       wrefresh(commandWindow);
     } else {
-      if (EditorMode == INSERT) {
-
+      if (editorMode == INSERT) {
         mvwprintw(commandWindow, 0, 0, "--INSERT--");
+        wrefresh(commandWindow);
+      } else {
+        mvwprintw(commandWindow, 0, 0,
+                  "MiniVim\t\t\t\tby Reqwey <reqwey05@sjtu.edu.cn>");
         wrefresh(commandWindow);
       }
       move(cursorY, cursorX);
@@ -370,8 +462,45 @@ private:
   }
 };
 
-int main() {
-  MiniVim editor;
+int main(int argc, char **argv) {
+  bool truncate = false;
+  bool readOnly = false;
+  WrapMode wrapMode = SCROLL;
+  std::string filename;
+
+  int opt;
+  while ((opt = getopt(argc, argv, "tRW:")) != -1) {
+    switch (opt) {
+    case 't':
+      truncate = true;
+      break;
+    case 'R':
+      readOnly = true;
+      break;
+    case 'W':
+      if (strcmp(optarg, "break") == 0) {
+        wrapMode = BREAK;
+      } else if (strcmp(optarg, "scroll") == 0) {
+        wrapMode = SCROLL;
+      } else {
+        fprintf(stderr, "Invalid argument for -W: %s\n", optarg);
+        return 1;
+      }
+      break;
+    default:
+      fprintf(stderr, "Usage: %s [-t] [-R] [-W break/scroll] <filename>\n",
+              argv[0]);
+      return 1;
+    }
+  }
+
+  if (optind >= argc) {
+    fprintf(stderr, "Expected filename argument\n");
+    return 1;
+  }
+
+  filename = argv[optind];
+  MiniVim editor(filename, truncate, readOnly, wrapMode);
   editor.run();
   return 0;
 }
